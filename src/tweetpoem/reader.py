ef0fb372e44fc4f407e48a1e8856493d284661f4
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-12 14:12:22 +0100

    no try/catch

--- src/tweetpoem/reader.py
+++ src/tweetpoem/reader.py
@@ -15,26 +15,19 @@ class StreamWatcherListener(tweepy.StreamListener):
 	SPACE = re.compile("[\\s\\.:,]+")
 
 	def on_status(self, status):
-
-		try:
-			article = False
-			for word in self.SPACE.split(status.text):
-				if word == '':
-					continue
-				if word == 'the':
-					article = True
-				else:
-					if article:
-						article = False
-						what = word.translate(None,'"?!\'@#()[]{}\\').lower()
-						if self.what != None and self.what != what:
-							print "I don't like the %s, i'd rather the %s" % (self.what, what)
-						self.what = what
-			#print '\n %s  %s  via %s\n' % (status.author.screen_name, status.created_at, status.source)
-		except:
-			# Catch any unicode errors while printing to console
-			# and just ignore them to avoid breaking application.
-			pass
+		article = False
+		for word in self.SPACE.split(status.text):
+			if word == '':
+				continue
+			if word == 'the':
+				article = True
+			else:
+				if article:
+					article = False
+					what = word.translate(None,'"?!\'@#()[]{}\\').lower()
+					if self.what != None and self.what != what:
+						print "I don't like the %s, i'd rather the %s" % (self.what, what)
+					self.what = what
 
 	def on_error(self, status_code):
 		print 'An error has occured! Status code = %s' % status_code
