45981da5061abb4338d6498060b4aaa03851bcc5
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-16 23:32:27 +0100

    time delta

--- src/tweetpoem/stack.py
+++ src/tweetpoem/stack.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
-import time
+from datetime import datetime, timedelta
 
 class Stack(object):
 	def __init__(self, maxage = 30):
 		"maxage in second"
-		self.maxage = maxage
+		self.maxage = timedelta(0, maxage)
+		self.birth = datetime.now()
 		self.data = {}
 		self.keys = []
+	def tick(self):
+		return datetime.now() - self.birth
 	def append(self, data):
-		tick = time.clock()
+		tick = self.tick()
 		self.data[tick] = data
 		self.keys.append(tick)
 	def __iter__(self):
-		now = time.clock()
+		now = self.tick()
 		for k in self.keys:
 			if now - k > self.maxage:
 				self.keys.remove(k)
@@ -31,8 +34,11 @@ class Stack(object):
 					yield self.data[k]
 
 if __name__ == '__main__':
-	s = Stack(30)
+	import time
+	s = Stack(3)
 	for a in range(20):
 		s.append(a)
 	assert range(20) == list(s)
 	print s.keys
+	time.sleep(5)
+	assert 0 == len(list(s))
