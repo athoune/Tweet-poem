8814b32fbd0611705fa6f1e7b5440a5adbc9e08e
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-17 21:34:34 +0100

    wait for data

--- src/tweetpoem/server.py
+++ src/tweetpoem/server.py
@@ -8,13 +8,14 @@ from tornado.escape import json_encode
 from datetime import timedelta
 from getpass import getpass
 import os
+import time
 
 import reader
 from stack import Stack
 
 def str2td(txt):
 	n = txt.split(':')
-	return timedelta(int(n[0], n[1], n[2]))
+	return timedelta(int(n[0]), int(n[1]), int(n[2]))
 def td2str(td):
 	return "%i:%i:%i" % (td.days, td.seconds, td.microseconds)
 
@@ -22,7 +23,7 @@ class StackBag(object):
 	def __init__(self, size=30):
 		self.stack = Stack(size)
 	def append(self, line):
-		print line
+		#print line
 		self.stack.append(line)
 
 class MainHandler(tornado.web.RequestHandler):
@@ -30,17 +31,25 @@ class MainHandler(tornado.web.RequestHandler):
 		self.render("index.html")
 
 class PoemHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
 	def get(self):
 		global stack
 		#self.set_header("Content-Type", "text/plain")
 		since = self.get_argument("since", None)
 		if since == None:
 			tick = stack.stack.tick()
-			data = list(stack.stack)
 		else:
 			tick = str2td(since)
-			data = list(stack.stack.since(tick))
-		self.write(json_encode({'tick': td2str(tick), 'data': data}))
+		while True:
+			if since == None:
+				data = list(stack.stack)
+			else:
+				data = list(stack.stack.since(tick))
+			if data != []:
+				break
+			time.sleep(1)
+		self.write(json_encode({'tick': td2str(tick), 'poems': data}))
+		self.finish()
 
 settings = {
 	"static_path": os.path.join(os.path.dirname(__file__), "data")
@@ -56,6 +65,7 @@ def main(login, password):
 	reader.poem(login, password, async=True, bag=stack)
 	http_server = tornado.httpserver.HTTPServer(application)
 	http_server.listen(8888)
+	print "open http://localhost:8888\n"
 	tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
