0cfb82eacfe6b8cd1333c53b647bbbee3e01453a
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-19 01:10:33 +0100

    more nice

--- src/tweetpoem/data/poem.js
+++ src/tweetpoem/data/poem.js
@@ -1,7 +1,53 @@
 var last = null;
 var poems;
 var size = 0;
-var MAX = 25;
+var MAX = 24;
+var stack = [];
+
+var position;
+var Circle = function(width, height) {
+  this.angle = 0;
+  this.width = width;
+  this.height = height;
+  this.radius = 300;
+};
+
+Circle.prototype = {
+  x: function() {
+    return (this.radius * Math.cos(this.angle)) + (this.radius);
+  },
+  y: function() {
+    return (this.radius * Math.sin(this.angle)) + (this.height/2);
+  },
+  next: function() {
+    this.angle += Math.PI / 15;
+  }
+};
+
+var Column = function(width, height) {
+  this.col = 0;
+  this.line = 0;
+};
+
+Column.prototype = {
+  x: function() {
+    return this.col * 350 + 10;
+  },
+  y: function() {
+    return this.line * 35 + 150;
+  },
+  next: function() {
+    this.line ++;
+    if(this.line > MAX / 3) {
+      this.line = 0
+      this.col++;
+    }
+    if(this.col >= 3) {
+      this.col = 0;
+    }
+    
+  }
+};
 
 var fetch = function() {
   $.ajax({
@@ -13,12 +59,7 @@ var fetch = function() {
     last = data.tick;
     if(typeof data.poems != 'undefined') {
       for(var i=0; i < data.poems.length; i++) {
-        poems.prepend($('<li>').text(data.poems[i]));
-        if(size > MAX) {
-          poems.children().last().remove();
-        } else {
-          size++;
-        }
+        stack.unshift(data.poems[i]);
       }
     }
     setTimeout('fetch()', 1000);
@@ -26,7 +67,34 @@ var fetch = function() {
   },
   cache: false});
 };
+
+var display = function() {
+  if(stack.length > 0) {
+    var poem = stack.pop();
+    poems.prepend(
+      $('<div>')
+      .text(poem)
+      .css('position', 'absolute')
+      .css('left', position.x())
+      .css('top' , position.y())
+/*      .fadeOut(100, function() {
+        $(this).remove();
+      })*/
+    );
+    position.next();
+    if(size > MAX) {
+      poems.children().last().remove();
+    } else {
+      size++;
+    }
+  }
+  setTimeout('display()', 100);
+}
+
 $(function(){
   poems = $('#poems');
+  var body = $('body');
+  position = new Column(body.width(), body.height());
   fetch();
+  display();
 });
