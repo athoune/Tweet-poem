1b84b89a800eadace197267354b26ba912478bc5
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-22 22:39:42 +0100

    tornado from redis

--- src/tweetpoem/server.py
+++ src/tweetpoem/server.py
@@ -5,50 +5,40 @@ import tornado.ioloop
 import tornado.web
 from tornado.escape import json_encode
 
-from datetime import timedelta
-from getpass import getpass
 import os
-import time
+import sys
 
-import reader
-from stack import Stack
-
-def str2td(txt):
-	n = txt.split(':')
-	return timedelta(int(n[0]), int(n[1]), int(n[2]))
-def td2str(td):
-	return "%i:%i:%i" % (td.days, td.seconds, td.microseconds)
-
-class StackBag(object):
-	def __init__(self, size=30):
-		self.stack = Stack(size)
-	def append(self, line):
-		#print line
-		self.stack.append(line)
+from redistack import RedisTack
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render("index.html")
+		return self.render('data/index.html')
 
 class PoemHandler(tornado.web.RequestHandler):
+	stack = RedisTack()
 	@tornado.web.asynchronous
 	def get(self):
-		global stack
 		self.set_header("Content-Type", "application/json")
 		since = self.get_argument("since", None)
-		if since == None:
-			tick = stack.stack.tick()
-		else:
-			tick = str2td(since)
 		while True:
 			if since == None:
-				data = list(stack.stack)
+				data = list(self.stack)
 			else:
-				data = list(stack.stack.since(tick))
+				data = list(self.stack.since(int(since)))
 			if data != []:
 				break
 			time.sleep(1)
-		self.write(json_encode({'tick': td2str(tick), 'poems': data}))
+		self.write('{"poems":[')
+		size = len(data)
+		cpt = 0
+		tick = None
+		for a in data:
+			cpt += 1
+			tick, poem = a.split(':', 1)
+			self.write(json_encode(poem))
+			if cpt < size:
+				self.write(',')
+		self.write('],"tick":%s}' %tick)
 		self.finish()
 
 settings = {
@@ -56,20 +46,15 @@ settings = {
 	"gzip": True
 }
 application = tornado.web.Application([
-    (r"/", MainHandler),
-	(r"/poem", PoemHandler),
+    (r"/", MainHandler, {}),
+	(r"/poem", PoemHandler)
 ], **settings)
 
-def main(login, password):
-	global stack
-	stack = StackBag(30)
-	reader.poem(login, password, async=True, bag=stack)
+def main(port):
 	http_server = tornado.httpserver.HTTPServer(application)
-	http_server.listen(8888)
-	print "open http://localhost:8888\n"
+	http_server.listen(port)
+	print "open http://localhost:%i\n" % port
 	tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-	username = raw_input('Twitter username: ')
-	password = getpass('Twitter password: ')
-	main(username, password)
+	main(int(sys.argv[1]))
